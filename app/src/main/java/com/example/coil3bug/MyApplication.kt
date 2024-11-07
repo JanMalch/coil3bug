c71d7fedb1285c5d041b34c536c8956b91932cd7
JanMalch <25508038+JanMalch@users.noreply.github.com>
2024-11-07 09:11:32 +0100

    FIX: add explicit ConnectivityChecker

--- app/src/main/java/com/example/coil3bug/MyApplication.kt
+++ app/src/main/java/com/example/coil3bug/MyApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import coil3.ImageLoader
 import coil3.SingletonImageLoader
 import coil3.annotation.ExperimentalCoilApi
+import coil3.network.ConnectivityChecker
 import coil3.network.cachecontrol.CacheControlCacheStrategy
 import coil3.network.okhttp.OkHttpNetworkFetcherFactory
 import coil3.util.DebugLogger
@@ -23,7 +24,8 @@ class MyApplication : Application() {
                             cacheStrategy = { CacheControlCacheStrategy() },
                             callFactory = {
                                 OkHttpClient()
-                            }
+                            },
+                            connectivityChecker = { ctx -> ConnectivityChecker(ctx) }
                         )
                     )
                 }
